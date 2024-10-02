cc3ced115416a31195aad4c71d15319ac96f1dc1
ChoiByunghyun <qhspsh123@naver.com>
2024-10-02 15:40:27 +0900

    1

---
+++ Test01/Test01/main.c
@@ -0,0 +1,18 @@
+/*
+ * Test01.c
+ *
+ * Created: 2024-10-02 오후 12:17:20
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code 고장나거나 전원이 꺼질 때 까지 계속실행되어야하므로 무한루프이다*/
+    while (1) 
+    {
+    }
+}
+
