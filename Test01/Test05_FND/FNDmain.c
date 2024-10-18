e93348dc152847e2f1341c4b0a8689d2c8d75e00
ChoiByunghyun <qhspsh123@naver.com>
2024-10-18 17:24:10 +0900

    dfa

--- Test01/Test05_FND/FNDmain.c
+++ Test01/Test05_FND/FNDmain.c
@@ -16,7 +16,7 @@ char data [] = {0, 0, 0, 0}; //1234
 	
 int digit(int num) // num 변수를 1000, 100, 10, 1 자릿수 추출하여 data[] 배열에 저장
 {
-	if(num > 9999) return 0;	//인수 유효성 체크	
+	if(num > 9999) return 0;	//인수 유효성 체크
 	data[3] = num % 10;//1
 	data[2] = (num / 10) % 10;//10
 	data[1] = (num / 100) % 10;//100
@@ -25,49 +25,124 @@ int digit(int num) // num 변수를 1000, 100, 10, 1 자릿수 추출하여 data
 }
 
 volatile int st = 0;
-ISR(INT0_vect)
-{
-	if(st == 0) st++;
-	else if(st == 1) st++;
-	else if(st > 1) st = 0;
-}
-
+volatile int st2 = 0;
 volatile int i = 0;
 volatile int cnt;
 volatile int num = 0;
+volatile int min = 0;
+volatile double num2 = 0;
+volatile Idx1 = 0;
+volatile int on = 0;
+volatile int TMOD = 0;
+volatile int TSET = 0;
+
+ISR(INT2_vect) // 버튼3 Timer 설정
+{
+	num = 0;
+	if(TMOD == 0) 
+	{TMOD = 1; Idx1 = 0;}
+	else TMOD = 0;
+
+	
+}
+ISR(INT0_vect) //버튼1 -- stop 기능
+{
+	if(TMOD == 0)
+	{
+		st2++;
+		if (st2 > 1) 
+		{
+			st2 = 0;
+			
+		}
+	}
+	else
+	{
+		if(Idx1 == 0) Idx1=1;
+		else Idx1 = 2;
+	}
+	
+}
+
+ISR(INT1_vect) //버튼 2
+{
+	if(Idx1 > 1)
+	{
+		on = 1;
+		Idx1 = 0;
+		TMOD = 0;
+	}
+	else if(Idx1 == 1)
+	{
+		num++;
+		if(num > 59) num = 0;
+	}
+	else if(st2 == 1 && TMOD == 0)
+	{
+		min++;
+		num = (min*100) + (num%100);
+		if(min > 59) min = 0;
+	}
+	
+}
+
+volatile int cnt2=0;
+
 ISR(TIMER0_COMP_vect)		//비교일치 인터럽트
 {
 	TCNT0 = 0;
 	cnt++;
 	if (cnt == 5)
-	{	
-		if (st == 0) num = 0;
-		if (st == 1) num++;
-		if (st == 2) ;
+	{
 		cnt = 0;
+		cnt2++;
+		if(cnt2 == 100)
+		{
+			if(TMOD == 0)
+			{
+				if(on == 1)
+				{
+					num--;
+					if(num == 0)
+					{
+						on=0;
+						TMOD;
+						st=0;
+						st2=0;
+						//GPIO- 부저 자리
+						for (int k = 0; k<100; k++)
+						{
+							
+						}
+					}
+				}
+				else if(st2 == 0)
+				{
+					if((num%100) == 59) num= num+40;
+					else num++;
+				}
+				if (st2 == 1) ;
+				
+			}
+			
+			cnt2 = 0;
+		}
+		
+		
 	}
-	//for(num = 0; num <10000; num++)
-	//{
-		//digit(num);
-		//for(int i = 0; i < 4; i++)
-		//{
-			//CPORT = ~_BV(i); //1 << i;
-			//{
-				//IPORT = img[data[i]];
-				//if(i == 2) IPORT |= 0x8F;
-				//_delay_ms(5);
-			//}
-			//
-		//}
-	//}
+	
 }
 volatile int j = 0;
 ISR(TIMER2_OVF_vect)
 {
-	digit(num);
+	if (st2 == 1) 
+	{
+		digit(num);
+	}
+	else digit(num);
 	CPORT = ~_BV(j); //1 << i;
 	IPORT = img[data[j]];
-	if (j == 1)	IPORT = IPORT + 0x80;
+	if (j == 1)	IPORT = IPORT + 0x80; //점 찍는 부분 0x08 = 1000 0000
 	j++;
 	if (j == 4) j = 0;
 }
@@ -83,14 +158,24 @@ int main(void)
 	IDDR |= 0xFF;		// 8bit 전체를 출력으로 설정
 	CPORT |= 0x00;
 	IPORT |= 0xBF;		
-	EIMSK |= _BV(INT0); // State(st) change interrupt 설정 INT0 활성화
-	EICRA |= 0x03;		// 상승에지에서 interrupt 요청 발생
+	EIMSK |= 0x07; // State(st) change interrupt 설정 INT0 활성화
+	EICRA |= 0x3F;		// 상승에지에서 interrupt 요청 발생
 	sei();
 	
 	int num;
     /* Replace with your application code */
     while (1)
-    {		
+    {	
+		if(st2 ==0)
+		{
+			TCCR0 |= 0x06;
+			OCR0 = 125 ; //125
+		}
+		else
+		{
+			TCCR0 |= 0x06;
+			OCR0 = 125;
+		}
 	    //for(num = 0; num <10000; num++)
 	    //{
 			//
